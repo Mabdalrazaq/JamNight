f94f80a0774cece0323ed46b3d584817f6a2ef6c
Mohammad Abdallah <m.abdalrazaq314@gmail.com>
2021-01-02 01:19:24 +0200

    refactored instrumentBuilder, removed unnecessary helper methods

--- src/main/java/database/InstrumentBuilder.java
+++ src/main/java/database/InstrumentBuilder.java
@@ -15,46 +15,25 @@ public class InstrumentBuilder {
 
   public static Instrument buildInstrumentFromJson(JSONObjectWrapper jsonInstrument) {
     return switch (InstrumentType.valueOf(jsonInstrument.getString("type").toUpperCase())) {
-      case GUITAR -> buildGuitarFromJson(jsonInstrument);
-      case PIANO -> buildPianoFromJson(jsonInstrument);
-      case BANJO -> buildBanjoFromJson(jsonInstrument);
-      case SYNTHESIZER -> buildSynthesizerFromJson(jsonInstrument);
-      case DRUMS -> buildDrumsFromJson(jsonInstrument);
+      case GUITAR -> new Guitar.Builder(jsonInstrument.getString("manufacturer"))
+          .setSizeOfString(jsonInstrument.getDouble("stringSize"))
+          .setNumOfStrings(jsonInstrument.getInt("stringsNum"))
+          .build();
+      case PIANO -> new Piano.Builder(jsonInstrument.getString("manufacturer"))
+          .setAcoustic(jsonInstrument.getBoolean("isAcoustic"))
+          .setNumOfKeys(jsonInstrument.getInt("keysNum"))
+          .build();
+      case BANJO -> new Banjo.Builder(jsonInstrument.getString("manufacturer"))
+          .setSizeOfString(jsonInstrument.getDouble("stringSize"))
+          .setNumOfStrings(jsonInstrument.getInt("stringsNum"))
+          .build();
+      case SYNTHESIZER -> new Synthesizer.Builder(jsonInstrument.getString("manufacturer"))
+          .setLed(jsonInstrument.getBoolean("hasLed"))
+          .setNumOfKeys(jsonInstrument.getInt("keysNum"))
+          .build();
+      case DRUMS -> new Drums.Builder(jsonInstrument.getString("manufacturer"))
+          .setHiHat(jsonInstrument.getBoolean("hasHiHat"))
+          .build();
     };
   }
-
-  private static Instrument buildGuitarFromJson(JSONObjectWrapper jsonInstrument) {
-    return new Guitar.Builder(jsonInstrument.getString("manufacturer"))
-        .setSizeOfString(jsonInstrument.getDouble("stringSize"))
-        .setNumOfStrings(jsonInstrument.getInt("stringsNum"))
-        .build();
-  }
-
-  private static Instrument buildPianoFromJson(JSONObjectWrapper jsonInstrument) {
-    return new Piano.Builder(jsonInstrument.getString("manufacturer"))
-        .setAcoustic(jsonInstrument.getBoolean("isAcoustic"))
-        .setNumOfKeys(jsonInstrument.getInt("keysNum"))
-        .build();
-  }
-
-  private static Instrument buildBanjoFromJson(JSONObjectWrapper jsonInstrument) {
-    return new Banjo.Builder(jsonInstrument.getString("manufacturer"))
-        .setSizeOfString(jsonInstrument.getDouble("stringSize"))
-        .setNumOfStrings(jsonInstrument.getInt("stringsNum"))
-        .build();
-  }
-
-  private static Instrument buildSynthesizerFromJson(JSONObjectWrapper jsonInstrument) {
-    return new Synthesizer.Builder(jsonInstrument.getString("manufacturer"))
-        .setLed(jsonInstrument.getBoolean("hasLed"))
-        .setNumOfKeys(jsonInstrument.getInt("keysNum"))
-        .build();
-  }
-
-  private static Instrument buildDrumsFromJson(JSONObjectWrapper jsonInstrument) {
-    return new Drums.Builder(jsonInstrument.getString("manufacturer"))
-        .setHiHat(jsonInstrument.getBoolean("hasHiHat"))
-        .build();
-  }
-
 }
